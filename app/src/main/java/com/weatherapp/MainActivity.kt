3e768368f0b7804d7e9a0e56f67914f8a1c51432
Gustavo Orlando <gustavoorlando.1d2015@gmail.com>
2026-05-06 14:42:47 -0300

    Prática 2 – Parte 1

--- app/src/main/java/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/weatherapp/MainActivity.kt
@@ -1,27 +1,12 @@
 package com.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
+import com.weatherapp.ui.HomePage
 import com.weatherapp.ui.theme.WeatherAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,31 +22,3 @@ class MainActivity : ComponentActivity() {
 
 
 
-@Composable
-fun HomePage() {
-    val activity = LocalContext.current as Activity
-    Scaffold { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-                .padding(24.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Bem-vindo!",
-                style = MaterialTheme.typography.headlineMedium
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            Button(
-                onClick = { activity.finish() },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = "Sair")
-            }
-        }
-    }
-}
